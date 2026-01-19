3ac27d5c456f1385c88e62465c44f7bc301afa72
choihooo <hochoi8621@naver.com>
2026-01-19 09:19:44 +0900

    refactor: 아이콘 컴포넌트 분리로 서버 컴포넌트 최적화
    - IconWrapper 클라이언트 컴포넌트 생성
    - Header, Footer, DesktopPlaceholder를 서버 컴포넌트로 전환
    - 나머지 컴포넌트들은 IconWrapper 사용으로 통일

---
+++ components/icons/IconWrapper.tsx
@@ -0,0 +1,8 @@
+"use client";
+
+import type { IconProps } from "@team-numberone/daepiro-design-system";
+import { Icon } from "@team-numberone/daepiro-design-system";
+
+export function IconWrapper(props: IconProps) {
+	return <Icon {...props} />;
+}
